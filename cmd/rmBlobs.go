9c1fc43af4a441cf48db2ffe3a4c8504ce748f94
Roger Keller <rokeller@users.noreply.github.com>
2024-06-07 15:16:16 +0200

    Add error handling for blobs rm command

--- cmd/rmBlobs.go
+++ cmd/rmBlobs.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blob"
+	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/bloberror"
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 )
@@ -17,16 +19,31 @@ var rmBlobsCmd = &cobra.Command{
 	Short: "Remove blobs",
 	Long:  "Remove blobs in a storage account's blob container",
 	Args:  cobra.MinimumNArgs(1),
-	Run:   runRmBlobs,
+	RunE:  runRmBlobs,
 }
 
-func runRmBlobs(cmd *cobra.Command, args []string) {
+func runRmBlobs(cmd *cobra.Command, args []string) error {
 	containerClient := execCtx.serviceClient.NewContainerClient(execCtx.containerName)
 
-	for _, arg := range args {
-		glog.Infof("remove blob: %s", arg)
-		blobClient := containerClient.NewBlobClient(arg)
+	for _, blobName := range args {
+		glog.Infof("remove blob: %s", blobName)
+		blobClient := containerClient.NewBlobClient(blobName)
 
-		blobClient.Delete(context.Background(), &blob.DeleteOptions{})
+		_, err := blobClient.Delete(context.Background(), &blob.DeleteOptions{})
+
+		if nil != err {
+			if bloberror.HasCode(err, bloberror.ContainerNotFound) {
+				err = fmt.Errorf("container \"%s\" does not exist", execCtx.containerName)
+			} else if bloberror.HasCode(err, bloberror.BlobNotFound) {
+				err = fmt.Errorf("blob \"%s\" does not exist in container \"%s\"",
+					blobName, execCtx.containerName)
+			}
+
+			return err
+		}
+
+		fmt.Fprintln(cmd.OutOrStdout(), blobName)
 	}
+
+	return nil
 }
